0bfd4b5a5945d4435aae80c67238642cf9a34e03
Tharllymartins <58658882+Tharllymartins@users.noreply.github.com>
2022-04-02 09:23:06 -0300

    added route to upload a avatar image to user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import cors from "cors";
-import { Router } from "express";
+import { response, Router } from "express";
 import CreateUserService from "../services/CreateUserService";
+import ensureAutheticated from "../middlewares/ensureAutheticated";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import UpdatedUserAvatarService from "../services/UpdatedUserAvatarService";
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 usersRouter.use(cors())
 
 
@@ -16,6 +21,22 @@ usersRouter.post("/", async ( req, res ) => {
             password
         })
         return res.status(201).json(user)
+    } catch (error) {
+        return res.json({msg: error})
+    }
+})
+
+usersRouter.patch('/avatar', ensureAutheticated, upload.single('avatar'), async (req, res) => {
+    try {
+        const updateUserAvatar = new UpdatedUserAvatarService();
+        const user = await updateUserAvatar.execute({
+            user_id: req.user.id,
+            avatarFileName: req.file?.filename
+        })
+        
+        delete user.password;
+
+        return res.json(user)
     } catch (error) {
         return res.json({msg: error})
     }

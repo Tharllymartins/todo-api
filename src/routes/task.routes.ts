3d60d615e4afeb1d6eb7e7be0d4a2728843e37b7
Tharlly Alves Martins <tharllymartins16@gmail.com>
2022-07-12 14:11:26 -0300

    added comments

--- src/routes/task.routes.ts
+++ src/routes/task.routes.ts
@@ -1,22 +1,23 @@
 import { Router } from "express";
 import ensureAutheticated from "../middlewares/ensureAutheticated";
-import { createSubTask, createTask, deleteSubTask, deleteTask, getTasks, updateSubTask, updateTask } from "../controller/taskController";
+import { createSubTask, createTag, createTask, deleteSubTask, deleteTask, getTags, getTasks, getTasksByTag, updateSubTask, updateTask } from "../controller/taskController";
 
 
 const taskRouter = Router();
-
-
 taskRouter.use(ensureAutheticated)
 
-
+// Route to get tasks
 taskRouter.get("/", getTasks);
-
+taskRouter.get("/tag", getTags)
+taskRouter.get("/tag/:tagId", getTasksByTag)
+// Routes to create data (task, subtask)
 taskRouter.post("/", createTask);
 taskRouter.post("/:taskId/subtask", createSubTask); 
-
+taskRouter.post("/tag", createTag);
+// Routes to update data (task, subtask)
 taskRouter.patch("/:id", updateTask);
 taskRouter.patch("/subtask/:id", updateSubTask);
-
+// Routes to delete data (task, subtask)
 taskRouter.delete("/:id", deleteTask);
 taskRouter.delete("/subtask/:id", deleteSubTask);
 

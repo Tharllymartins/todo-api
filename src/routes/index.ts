a90c3e96f2c37be6dfd4da4dd13769f1b30e1ad3
Tharlly Alves Martins <tharllymartins16@gmail.com>
2022-07-13 19:57:53 -0300

    removed tag router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import tagRouter from "./tag.routes";
 import todoRouter from "./task.routes"
 import usersRouter from "./users.routes";
 import cors from "cors";
@@ -9,7 +8,6 @@ const routes = Router();
 routes.use(cors());
 routes.use("/task", todoRouter);
 routes.use("/user", usersRouter);
-routes.use("/tag", tagRouter);
 
 
 
